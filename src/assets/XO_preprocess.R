85f6063503d7b7739e60d1f3644cd95e011f4ea2
Giulio Benedetti <giuliobene2000@gmail.com>
2022-04-04 11:16:56 +0300

    Document import functions

--- src/assets/XO_preprocess.R
+++ src/assets/XO_preprocess.R
@@ -2,15 +2,9 @@ library(microbiomeDataSets)
 
 mae <- HintikkaXOData()
 
-coldata <- colData(mae)
-
 se1 <- mae[["microbiota"]]
 se2 <- mae[["metabolites"]]
 se3 <- mae[["biomarkers"]]
-
-rowdata1 <- rowData(se1)
-rowdata2 <- rowData(se2)
-rowdata3 <- rowData(se3)
 
 microbiota_assays <- assays(se1)
 metabolites_assays <- assays(se2)
@@ -20,8 +14,6 @@ write.csv(microbiota_assays, "XO_microbiota_assays.csv")
 write.csv(metabolites_assays, "XO_metabolites_assays.csv")
 write.csv(biomarkers_assays, "XO_biomarkers_assays.csv")
 
-write.csv(coldata, "sample_data.csv",
+write.csv(colData(mae), "XO_sample_data.csv")
+write.csv(sampleMap(mae),"XO_sample_map.csv",
           row.names = FALSE)
-write.csv(sampleMap(mae),"sample_map.csv",
-          row.names = FALSE)
-write.csv(assays(se1), "assays_test.csv")
--- src/importers.jl
+++ src/importers.jl
@@ -1,3 +1,16 @@
+"""
+    import_se_from_csv(assays_file::AbstractString, rowdata_file::AbstractString, coldata_file::AbstractString)
+
+Imports a SummarizedExperiment object from its components stored as csv files.
+
+# Arguments
+- `assays_file::AbstractString`: the path to a csv file containing the assays.
+   It is produced in R with `write.csv(assays(se), "assays_file.csv")`.
+- `rowdata_file::AbstractString`: the path to a csv file containing the rowdata.
+   It is produced in R with `write.csv(rowData(se), "rowdata_file.csv")`.
+- `coldata_file::AbstractString`: the path to a csv file containing the coldata.
+   It is produced in R with `write.csv(colData(se), "coldata_file.csv")`.
+"""
 function import_se_from_csv(assays_file::AbstractString, rowdata_file::AbstractString, coldata_file::AbstractString)
 
     raw_assays = CSV.File(assays_file) |> DataFrame
@@ -16,6 +29,20 @@ function import_se_from_csv(assays_file::AbstractString, rowdata_file::AbstractS
 
 end
 
+"""
+    import_se_from_csv(assays_file::AbstractString)
+
+Imports a SummarizedExperiment object from a csv file containing the assays.
+Rowdata and coldata are generated based on the information stored in the row
+and column names of `assays_file`. This is the method of choice if the rowdata
+and coldata of the original SummarizedExperiment object does not provide valuable
+information either because they are empty or because they are part of a
+MultiAssayExperiment object.
+
+# Arguments
+- `assays_file::AbstractString`: the path to a csv file containing the assays.
+   It is produced in R with `write.csv(assays(se), "assays_file.csv")`.
+"""
 function import_se_from_csv(assays_file::AbstractString)
 
     raw_assays = CSV.File(assays_file) |> DataFrame
@@ -35,6 +62,26 @@ function import_se_from_csv(assays_file::AbstractString)
 
 end
 
+"""
+    import_mae_from_csv(experiment_files::Vector{<:AbstractString}, sample_data_file::AbstractString, sample_map_file::AbstractString; experiment_names::Vector{<:AbstractString} = experiment_files)
+
+Imports a MultiAssayExperiment object from its components stored as csv files.
+
+# Arguments
+- `experiment_files::Vector{<:AbstractString}`: a list of paths to the csv files
+   containing the assays of different experiments. For example, if a MultiAssayExperiment
+   object has 3 experiments, `experiment_files` is defined as:
+  `[assays_file1, assays_file2, assays_file3]`
+   Each `assays_file` is produced in R with `write.csv(assays(se), "assays_file.csv")`.
+   It is produced in R with `write.csv(assays(se), "assays_file.csv")`.
+- `sample_data_file::AbstractString`: the path to a csv file containing the sample data.
+   It is produced in R with `write.csv(colData(mae), "sample_data_file.csv")`.
+- `sample_map_file::AbstractString`: the path to a csv file containing the sample map.
+   It is produced in R with `write.csv(colData(mae), "sample_map_file.csv", row.names = FALSE)`.
+- `experiment_names::Vector{<:AbstractString}`: a vector of names to assign to the
+   experiments listed in `experiment_files`. By default, the experiments are named
+   with the filepaths specified in `experiment_files`.
+"""
 function import_mae_from_csv(experiment_files::Vector{<:AbstractString}, sample_data_file::AbstractString, sample_map_file::AbstractString; experiment_names::Vector{<:AbstractString} = experiment_files)
 
     experiment_list = OrderedDict{String, SummarizedExperiment}()
@@ -46,7 +93,7 @@ function import_mae_from_csv(experiment_files::Vector{<:AbstractString}, sample_
     end
 
     sample_data = CSV.File(sample_data_file) |> DataFrame
-    rename!(sample_data, Dict(:Sample => "name"))
+    rename!(sample_data, Dict(:Column1 => "name"))
 
     sample_map = CSV.File(sample_map_file) |> DataFrame
     rename!(sample_map, Dict(:assay => "experiment"))
@@ -68,7 +115,7 @@ end
 #     joinpath(@__DIR__, "assets/sample_map.csv"),
 #     experiment_names = experiment_names)
 
-function construct_assays(raw_assays, groups)
+function construct_assays(raw_assays::DataFrame, groups::Vector{<:AbstractArray})
 
     assays = OrderedDict{String, AbstractArray}()
 
