4be982a2eeecbfc5fca3ded3caad91fd140620fb
Giulio Benedetti <giuliobene2000@gmail.com>
2022-04-27 10:59:53 +0900

    Copy se.rowdata instead of modifying it in sorters.jl

--- src/sorters.jl
+++ src/sorters.jl
@@ -11,7 +11,7 @@ Sorts rows / features by one specific assay and returns the top elements.
 """
 function select_top_taxa(se::SummarizedExperiment, assay_name::String; top_n::Int64 = 10, sel_method::Function = mean)
 
-    features = se.rowdata
+    features = copy(se.rowdata)
     features[!, Symbol(assay_name)] = [sel_method(row) for row in eachrow(assay(se, assay_name))]
 
     sort(dropmissing(features, Symbol(assay_name)), Symbol(assay_name), rev = true)[1:top_n, :]
@@ -31,7 +31,7 @@ Sorts rows / features by multiple assays and returns the top elements.
 """
 function select_top_taxa(se::SummarizedExperiment, assay_names::Vector{String}; top_n::Int64 = 10, sel_method::Function = mean)
 
-    features = se.rowdata
+    features = copy(se.rowdata)
 
     for ass in assay_names
         
